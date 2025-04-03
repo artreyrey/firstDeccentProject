38cf555977a1a54b7e9c99bc974bcaaf9dcaeba4
Art reyrey <cuizonlance49@gmail.com>
2025-04-04 00:17:29 +0800

    ds

--- COSyM/landingPage/landingPage.js
+++ COSyM/landingPage/landingPage.js
@@ -1,23 +1,18 @@
 let sections = document.querySelectorAll('section')
 let navLinks = document.querySelectorAll('header nav a')
 
-
-
 window.onscroll = () => {
-    section.forEach(sec=>{
+    sections.forEach(sec => {
         let top = window.scrollY;
-        let offset = sec.offsetTop;
+        let offset = sec.offsetTop - 150; // Added offset to trigger earlier
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-
-        if(top >= offset && top < offset + height){
+        if(top >= offset && top < offset + height) {
             navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a [href*=' + id + ']').classList.add('active');
-
+                document.querySelector('header nav a[href="#' + id + '"]').classList.add('active');
             });
-        };
-
+        }
     });
-}
+};
