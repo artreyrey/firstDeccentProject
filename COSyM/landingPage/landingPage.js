dcb0c245f97aa82fe7e35c67150d13819b529468
Art reyrey <cuizonlance49@gmail.com>
2025-04-05 18:18:25 +0800

    ds

--- COSyM/landingPage/landingPage.js
+++ COSyM/landingPage/landingPage.js
@@ -1,17 +1,19 @@
+/*-- pagnagsscroll magiiba kulay ng hover ng current section--*/
+
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
-        let offset = sec.offsetTop - 150; 
+        let offset = sec.offsetTop - 150; /*-- masmaaga umilaw habang di pa fully nasscroll--*/
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height) {
+        if(top >= offset && top < offset + height) {/*-- check kung nasa current boundary ang section--*/
             navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+                document.querySelector('header nav a[href*=' + id + ']') /*-- Finds the link whose href contains the current section's ID and adds 'active' class to it--*/
             });
         };
     });
