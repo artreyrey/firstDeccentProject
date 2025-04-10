5379b5e6527e4e3d9d814462a2f85fe98f40a886
Art reyrey <cuizonlance49@gmail.com>
2025-04-11 01:44:20 +0800

    ds

--- COSyM/landingPage/landingPage.js
+++ COSyM/landingPage/landingPage.js
@@ -56,3 +56,12 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault(); // Remove this if it exists
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
