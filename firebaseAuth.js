b5b2e5129bcdba9f67ea5d1f78324267c9b4a237
Art reyrey <cuizonlance49@gmail.com>
2025-04-12 00:07:49 +0800

    ds

---
+++ firebaseAuth.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCNVoM7hQ6a1zcP5zDITcdmUKlfs6lcDBY",
+  authDomain: "login-form-783e1.firebaseapp.com",
+  projectId: "login-form-783e1",
+  storageBucket: "login-form-783e1.firebasestorage.app",
+  messagingSenderId: "598925515666",
+  appId: "1:598925515666:web:b16534b6158c7232a47f4b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
