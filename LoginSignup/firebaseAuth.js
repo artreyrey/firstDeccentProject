53eef4c3fdc97ffbac3425d90de92cb2d573628f
Art reyrey <cuizonlance49@gmail.com>
2025-04-14 01:36:17 +0800

    d

--- LoginSignup/firebaseAuth.js
+++ LoginSignup/firebaseAuth.js
@@ -18,6 +18,16 @@
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
 
+  function showMessage(message, divId){
+    var messageDiv = document.getElementById(divId);
+    messageDiv.style.display="bllock";
+    messageDiv.innerHTML=message;
+    messageDiv.style.opacity=1;
+    setTimeout(function(){
+      messageDiv.style.opacity=0;
+    }, 5000);
+  }
+
   // signup functions when clicked
   const signUp = document.getElementById('submitSignUp');
   
