1a83018f8d4f0921628c9995be506c93db192eab
Art reyrey <cuizonlance49@gmail.com>
2025-04-14 03:04:05 +0800

    Update firebaseAuth.js

--- LoginSignup/firebaseAuth.js
+++ LoginSignup/firebaseAuth.js
@@ -93,13 +93,3 @@ signIn.addEventListener('click', (event)=>{ // When clicked:
   })
   
 })
-
-// Function to check if user is logged in (call this on homePage.html)
-function checkAuth() {
-  const auth = getAuth();
-  const user = auth.currentUser;
-  
-  if (!user && !localStorage.getItem('loggedInUserId')) {
-    window.location.href = 'loginSignup.html'; // Redirect to login if not authenticated
-  }
-}
