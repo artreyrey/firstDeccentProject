6dfa4fdf32130ca7a447f24baf403b0df4ecfded
Art reyrey <cuizonlance49@gmail.com>
2025-04-14 01:16:55 +0800

    d

--- LoginSignup/firebaseAuth.js
+++ LoginSignup/firebaseAuth.js
@@ -1,6 +1,7 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
-  import {getAuth, createUserWithEmailAndPassowrd, signInWithEmailAndPassword} from "firebase/app"
+  import {getAuth, createUserWithEmailAndPassowrd, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/11.6.0/firebase-Auth.js";
+  import {getFirestore, setDoc, doc} from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
