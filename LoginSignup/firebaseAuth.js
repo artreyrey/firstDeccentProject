cba316919f36a11f833af9cb360bf1dc5fc7f789
Art reyrey <cuizonlance49@gmail.com>
2025-04-14 01:10:46 +0800

    d

--- LoginSignup/firebaseAuth.js
+++ LoginSignup/firebaseAuth.js
@@ -1,18 +1,18 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+  import {getAuth, createUserWithEmailAndPassowrd, signInWithEmailAndPassword} from "firebase/app"
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// Firebase settings (like an address to connect to your Firebase project)
-const firebaseConfig = {
-  apiKey: "AIzaSyCNVoM7hQ6a1zcP5zDITcdmUKlfs6lcDBY", // Unique key for your project
-  authDomain: "login-form-783e1.firebaseapp.com",    // Where users log in
-  projectId: "login-form-783e1",                     // Your Firebase project ID
-  storageBucket: "login-form-783e1.firebasestorage.app", // For storing files (images, etc.)
-  messagingSenderId: "598925515666",                 // For sending notifications
-  appId: "1:598925515666:web:b16534b6158c7232a47f4b" // Unique app identifier
-};
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCNVoM7hQ6a1zcP5zDITcdmUKlfs6lcDBY",
+    authDomain: "login-form-783e1.firebaseapp.com",
+    projectId: "login-form-783e1",
+    storageBucket: "login-form-783e1.firebasestorage.app",
+    messagingSenderId: "598925515666",
+    appId: "1:598925515666:web:b16534b6158c7232a47f4b"
+  };
 
-// Start Firebase with the given settings
-const app = initializeApp(firebaseConfig);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
