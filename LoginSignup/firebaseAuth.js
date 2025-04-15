eb7e80aac58202063d29d164b0d8c5e04f0947e5
Art reyrey <cuizonlance49@gmail.com>
2025-04-15 20:22:47 +0800

    d

--- LoginSignup/firebaseAuth.js
+++ LoginSignup/firebaseAuth.js
@@ -134,17 +134,11 @@ googleLogin.addEventListener("click", function(){
     const credential = GoogleAuthProvider.credentialFromResult(result);
     const token = credential.accessToken;
     const user = result.user;
-    // IdP data available using getAdditionalUserInfo(result)
-    // ...
   }).catch((error) => {
-    // Handle Errors here.
     const errorCode = error.code;
     const errorMessage = error.message;
-    // The email of the user's account used.
     const email = error.customData.email;
-    // The AuthCredential type that was used.
     const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
   });
  });
 
