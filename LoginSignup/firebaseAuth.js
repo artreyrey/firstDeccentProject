a96ea955836e47ceec1108af9255d1a7ec4b9db5
Art reyrey <cuizonlance49@gmail.com>
2025-04-15 20:27:51 +0800

    d

--- LoginSignup/firebaseAuth.js
+++ LoginSignup/firebaseAuth.js
@@ -133,14 +133,15 @@ googleLogin.addEventListener("click", function(){
   .then((result) => {
     const credential = GoogleAuthProvider.credentialFromResult(result);
     const user = result.user;
-
+    console.log(user);
+    window.location.href="";
 
   }).catch((error) => {
     const errorCode = error.code;
     const errorMessage = error.message;
   });
 
-  
+
  })
 
 
