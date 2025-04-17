51172b704913c8553fdfbb3450221dc1de12756c
Art reyrey <cuizonlance49@gmail.com>
2025-04-17 23:14:02 +0800

    s

--- LoginSignup/firebaseAuthAndAccountHandling.js
+++ LoginSignup/firebaseAuthAndAccountHandling.js
@@ -140,7 +140,7 @@ googleLogin.addEventListener("click", function(){
     const errorCode = error.code;
     const errorMessage = error.message;
   });
- }) 
+ })  
 
 // Facebook Login
 const facebookProvider = new FacebookAuthProvider();
