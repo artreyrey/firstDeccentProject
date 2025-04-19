12d8a51a18201a8ef5a49f67184bfacebe979033
Art reyrey <cuizonlance49@gmail.com>
2025-04-20 01:16:19 +0800

    d

--- COSyM/LoginSignup/firebaseAuth.js
+++ COSyM/LoginSignup/firebaseAuth.js
@@ -138,4 +138,4 @@ const googleAuth = () => {
 };
 
 document.getElementById("google-login-btn")?.addEventListener("click", googleAuth);
-document.getElementById("signUpButton")?.addEventListener("click", googleAuth);
+document.getElementById("sign-up-button")?.addEventListener("click", googleAuth);
