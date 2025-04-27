73b804c4f1075685a70608e1ab6f17ea5d46a632
Art reyrey <cuizonlance49@gmail.com>
2025-04-27 10:52:34 +0800

    d

--- public/COSyM/homePage/homePage.js
+++ public/COSyM/homePage/homePage.js
@@ -120,30 +120,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 // Profile section edit adn display connected to
 // Firebase configuration 
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
-  import {getAuth}
-  from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
-  import {getFirestore, setDoc, doc} 
-  from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
 
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyCNVoM7hQ6a1zcP5zDITcdmUKlfs6lcDBY",
-    authDomain: "login-form-783e1.firebaseapp.com",
-    projectId: "login-form-783e1",
-    storageBucket: "login-form-783e1.firebasestorage.app",
-    messagingSenderId: "598925515666",
-    appId: "1:598925515666:web:b16534b6158c7232a47f4b"
-  };
-
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth(app);
-  const db = getFirestore();
-  auth.languageCode = 'en';
 
 //profile set up
 const editButton = document.getElementById('editButton');
@@ -170,6 +147,9 @@ const displayEmail = document.getElementById('displayEmail');
 const displayStatus = document.getElementById('displayStatus');
 
 // Edit button click handler
+displayForm.style.display = "flex";
+editForm.style.display = "none";
+
 editButton.addEventListener('click', function() {
     // Copy current display values to edit form
     editName.value = displayName.textContent;
