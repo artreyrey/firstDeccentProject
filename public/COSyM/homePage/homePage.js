43d1268c52cd25f46d273c53671bea9d519ebc36
Art reyrey <cuizonlance49@gmail.com>
2025-04-29 19:53:55 +0800

    d

--- public/COSyM/homePage/homePage.js
+++ public/COSyM/homePage/homePage.js
@@ -118,9 +118,27 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
-// Profile section edit adn display connected to
+// Profile section edit adn display connected to firebase data base
 // Firebase configuration 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendPasswordResetEmail, GoogleAuthProvider, signInWithPopup} 
+from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
+import {getFirestore, setDoc, doc} 
+from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
 
+const firebaseConfig = {
+    apiKey: "AIzaSyCNVoM7hQ6a1zcP5zDITcdmUKlfs6lcDBY",
+    authDomain: "login-form-783e1.firebaseapp.com",
+    projectId: "login-form-783e1",
+    storageBucket: "login-form-783e1.firebasestorage.app",
+    messagingSenderId: "598925515666",
+    appId: "1:598925515666:web:5acb6fa146b160cca47f4b"
+  };
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore();
+auth.languageCode = 'en';
 
 //profile set up
 const editButton = document.getElementById('editButton');
