61c22abc1f794325c9e879a47e448d4dd83c5b8f
Art reyrey <cuizonlance49@gmail.com>
2025-05-04 01:39:32 +0800

    s

--- public/COSyM/homePage/profileSettingsModule.js
+++ public/COSyM/homePage/profileSettingsModule.js
@@ -17,7 +17,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyCNVoM7hQ6a1zcP5zDITcdmUKlfs6lcDBY",
     authDomain: "login-form-783e1.firebaseapp.com",
     projectId: "login-form-783e1",
-    storageBucket: "login-form-783e1.appspot.com",
+    storageBucket: "login-form-783e1.firebasestorage.app",
     messagingSenderId: "598925515666",
     appId: "1:598925515666:web:5acb6fa146b160cca47f4b"
 };
