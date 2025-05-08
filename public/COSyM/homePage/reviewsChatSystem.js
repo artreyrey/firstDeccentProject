094c633a3a4d26369a311de44fd59a612e1e38dd
Art reyrey <cuizonlance49@gmail.com>
2025-05-08 22:29:01 +0800

    s

--- public/COSyM/homePage/reviewsChatSystem.js
+++ public/COSyM/homePage/reviewsChatSystem.js
@@ -1,6 +1,6 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
-  import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendPasswordResetEmail, GoogleAuthProvider, signInWithPopup} 
+  import {getAuth} 
   from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
   import {getFirestore, setDoc, doc} 
   from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
